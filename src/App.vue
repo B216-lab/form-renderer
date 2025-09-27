093afe519902b29970e21df1fb69b958d8361c25
volard <kirillzhilenkov@gmail.com>
2025-09-28 02:14:12 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import DayMovements from './components/DayMovements.vue'
+
+export default {
+  components: {
+    DayMovements
+  }
+}
+</script>
+
+<template>
+  <DayMovements />
+</template>
---
+++ src/components/DayMovements.vue
@@ -0,0 +1,16 @@
+<script>
+import { useVueform, Vueform } from '@vueform/vueform'
+import { schema } from './schema'
+
+export default {
+  mixins: [Vueform],
+  setup: useVueform,
+  data: () => ({
+    vueform: {
+      size: 'md',
+      displayErrors: false,
+      schema: schema,
+    }
+  })
+}
+</script>
