d408245e9e288746ed72d972c2b823b62476ac9d
volard <kirillzhilenkov@gmail.com>
2025-09-28 17:26:58 +0800

    feat: Add env vars acquirement

--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,27 @@ export const devlog = (message: any) => {
     console.log(message);
   }
 };
+
+/**
+ * Перечисление для ключей переменных окружения, связанных с Dadata
+ */
+export enum EnvKey {
+    ApiKey = "VITE_DADATA_KEY",
+    ApiUrl = "VITE_DADATA_API"
+}
+
+/**
+ * Получает значение переменной окружения по ключу из DadataEnvKey
+ *
+ * :param key: ключ из перечисления DadataEnvKey
+ * :raises: Error если переменная не установлена
+ * :return: строка с значением переменной окружения
+ */
+export function getEnvValue(key: EnvKey): string {
+    const value = import.meta.env[key];
+    if (!value) {
+        devlog(`ENV variable "${key}" is not set`);
+        throw new Error(`ENV variable "${key}" is not set`);
+    }
+    return value;
+}
