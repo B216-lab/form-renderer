70af0f80675bbb55980fb9ed5199e33bcdd78537
volard <kirillzhilenkov@gmail.com>
2025-10-10 00:14:21 +0800

    refactor(DayMovements): update form setup to use composition API and ref for schema

--- src/components/DayMovements.vue
+++ src/components/DayMovements.vue
@@ -1,16 +1,20 @@
 <script>
+import { ref } from 'vue'
 import { useVueform, Vueform } from '@vueform/vueform'
 import { schema } from './schema'
 
 export default {
   mixins: [Vueform],
-  setup: useVueform,
-  data: () => ({
-    vueform: {
-      size: 'md',
-      displayErrors: false,
-      schema: schema,
+  setup(props, context)
+  {
+    const form = useVueform(props, context)
+
+    const vueform = ref(schema)
+
+    return {
+      ...form,
+      vueform,
     }
-  })
+  }
 }
 </script>
