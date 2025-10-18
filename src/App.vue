907c10eeac4935f68d4b4d9f371e3b49bca1b5c0
volard <kirillzhilenkov@gmail.com>
2025-10-18 19:38:34 +0800

    refactor: move out test code and use created form

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,7 @@
 <template>
-  <Vueform :prepare="prepare" ref="form$" v-model="data" sync>
-    <SelectElement label="Адрес проживания" name="address" :search="true" :delay="ADDRESS_DELAY" :filter-results="false"
-      :items="getAddressItems" />
-
-    <SelectElement label="Другой адрес проживания" name="address_other" :search="true" :delay="ADDRESS_DELAY"
-      :filter-results="false" :items="getAddressItems" />
-    <ButtonElement name="submit" button-label="Submit" submits />
-  </Vueform>
+  <DayMovements />
 </template>
 
 <script setup lang="ts">
-import { useDaDataAddress } from '@/daDataService/useDaDataAddress'
-import { computed } from 'vue'
-import { useFormsStore } from '@/forms/formDataStore'
-import type { Vueform } from '@vueform/vueform'
-
-const prepare = (form$: Vueform) => {
-  console.log(form$.data)
-}
-
-
-const store = useFormsStore()
-
-const data = computed({
-  get: () => store.form,
-  set: (data) => store.form = data
-})
-
-
-const { getAddressItems, ADDRESS_DELAY } = useDaDataAddress(3)
+import DayMovements from './forms/DayMovements.vue'
 </script>
