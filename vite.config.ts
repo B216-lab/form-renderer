90e38e5ed9b51edaa6f9de51794b148fb85fa35c
volard <kirillzhilenkov@gmail.com>
2025-11-28 00:12:50 +0800

    #1288065 fix: local dev tools with localStorage conflict

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import path from 'node:path'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
+import path from 'node:path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
   server: {
-        host: true,
-        port: 5173
-    }
-})
+    host: true,
+    port: 5173,
+  },
+});
